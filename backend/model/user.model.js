5ce79e98e77a616bdb8ec819485b377fcdbf2bf9
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2023-12-28 17:55:19 +0530

    first commit

---
+++ backend/model/user.model.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+
+const userSchema=mongoose.Schema(
+    {
+        name:String,
+        username:String,
+        email:String,
+        phone:String,
+        password:String
+    },
+    {
+        versionKey:false
+    }
+)
+
+const userModel=mongoose.model("user",userSchema);
+
+module.exports={userModel}
+
